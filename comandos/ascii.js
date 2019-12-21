6f55b1a49a2ae2f4a50916b1c82801900a686ec1
goncalo001 <50272342+goncalo001@users.noreply.github.com>
2019-12-21 03:23:30 +0000

    Update ascii.js

--- comandos/ascii.js
+++ comandos/ascii.js
@@ -8,10 +8,8 @@ const ascii = require("ascii-art");
 module.exports.run = async (bot, message, args, ops) => {
     if(!args.join(' ')) return message.channel.send("É necessário escrever algum texto para converter para ascii.").then(m => m.delete(2000));
     
-    ascii.font(args.join(' '), 'Doom', function(rendered) {
+    ascii.font(args.join(' '), 'Doom', async function(rendered) {
        console.log(rendered)
-
-       if(rendered.length > 2000) return message.channel.send("Essa mensagem é muito longa.").then(m => m.delete(2000));
 
        message.channel.send(rendered, {
            code: 'md'
