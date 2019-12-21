b71ea7433f4a5569b0a2f8508036456dfd6dc7c7
goncalo001 <50272342+goncalo001@users.noreply.github.com>
2019-12-21 03:48:35 +0000

    Update ascii.js

--- comandos/ascii.js
+++ comandos/ascii.js
@@ -1,15 +1,19 @@
 const Discord = require("discord.js");
 const botconfig = require("../botconfig.json");
 const cores = require("../cores.json");
-const ascii = require("ascii-art");
+const Font = require('ascii-art-table');
 
 
 
 module.exports.run = async (bot, message, args, ops) => {
+
+    Font.fontPath = 'Fonts'
+
     if(!args.join(' ')) return message.channel.send("É necessário escrever algum texto para converter para ascii.").then(m => m.delete(2000));
     
-    ascii.font(args.join(' '), 'Doom', async function(rendered) {
-       console.log(rendered)
+    Font.create(args.join(' '), 'Doom', async function(rendered) {
+
+        console.log(rendered)
 
        message.channel.send(rendered, {
            code: 'md'
