d28344e768c590e1a58e408b5a138859e98c9950
goncalo001 <50272342+goncalo001@users.noreply.github.com>
2019-12-26 04:40:07 +0000

    Create minecraftachievement Update ajuda.js

---
+++ comandos/minecraftachivement.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const cores = require("../cores.json");
+const MCA = require('minecraft-achievement');
+
+module.exports.run = async (bot, message, args) => {
+    let args = message.content.split(" ");
+
+    var url = MCA.url({
+        title: args[0],
+        body: args[1],
+        icon: args[2]
+    })
+
+    message.channel.send(url);
+
+}
+
+
+module.exports.config = {
+    name: "minecraftachievement",
+    description: "envia um achievement do minecraft.",
+    usage: "! + minecraftachievement + (nome do ícone) + (texto)",
+    aliases: ["MCA", "mcachivement"],
+    accessablelby: "Membros"
+}
