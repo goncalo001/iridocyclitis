d6c37ad06ff4da7598a3adeeaf30b2e39e268945
goncalo001 <50272342+goncalo001@users.noreply.github.com>
2019-12-26 02:16:31 +0000

    Created google.js

---
+++ comandos/google.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const cores = require("../cores.json");
+const Google = require('relevant-google');
+const googleAPIKey = (process.env.googleAPI);
+
+module.exports.run = async (bot, message, args) => {
+    const fetch = require('node-fetch');
+
+    const queryString = require('querystring');
+
+    const google = new Google(googleAPIKey);
+
+    if(!args.length) {
+        return message.channel.send('É necessário fornecer um termo de pesquisa.').then(m => m.delete(2000));
+    }
+
+    const query = queryString.stringify({term: args.join(' ')});
+
+    const { list } = await fetch(google.search(query).then(res => {
+        let embed = new Discord.RichEmbed()
+        .setColor(cores.azul)
+        .setTitle(`${res.title}`)
+        .setURL(`${res.formattedUrl}`)
+        .addField(`Resultados Google:` (res.snippet))
+        .addField(`Link:`(`${res.formattedUrl}`))
+        message.channel.send(embed)
+    }))
+
+    if(!list.length) {
+        message.channel.send(`Não existem resultados para a pesquisa ${args.join(" ")}`);
+    }
+ 
+    
+    
+   
+
+}
+
+    
+    
+   
+    
+
+
+module.exports.config = {
+    name: "google",
+    description: "pesquisa resultados diretamente da google.",
+    usage: "! + google + (termo de pesquisa)",
+    aliases: ["pesquisa", "pgoogle"],
+    accessablelby: "Membros"
+}
