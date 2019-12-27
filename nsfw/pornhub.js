8e86f3647dd32a6aaae765d5856e1dbaceaa0817
goncalo001 <50272342+goncalo001@users.noreply.github.com>
2019-12-27 21:33:39 +0000

    Update pornhub.js

--- nsfw/pornhub.js
+++ nsfw/pornhub.js
@@ -7,10 +7,11 @@ module.exports.run = async (bot, message, args) => {
     if (!query) return message.channel.send("É necessário introduzir um termo de pesquisa.").then(m => m.delete(2000));
     if (!message.channel.nsfw) return message.channel.send("É necessário que esteja num canal nsfw para poder usar este comando.").then(m => m.delete(2000));
     const Pornsearch = require('pornsearch').search(query);
-    Pornsearch.driver('sex');
+    Pornsearch.driver('sex')
         Pornsearch.gifs(1)
             .then(gifs => {
                 let gifrnd = gifs.map(gif => gif.url)
+                console.log(gifs)
                 let embed = new Discord.RichEmbed()
                     .setColor(cores.azul)
                     .setImage(gifrnd[Math.floor(Math.random() * gifrnd.length)])
